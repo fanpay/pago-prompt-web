48c9876984015ab6fbe3a27e63a5d7cb1bf4c882
Fabian Payan <f.payan@uniandes.edu.co>
2024-03-09 20:23:34 -0500

    Se crea primer routing
    Se crea primer routing de la aplicación para separar flujos
    
    Co-Authored-By: Esneider Velandia Gonzalez <e.velandia2164@uniandes.edu.co>

--- pago-prompt-web/src/app/alarms/alarms.component.ts
+++ pago-prompt-web/src/app/alarms/alarms.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-alarms',
   templateUrl: './alarms.component.html',
   styleUrl: './alarms.component.scss'
 })
-export class AlarmsComponent {
+export class AlarmsComponent implements OnInit {
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) { }
+
+  ngOnInit() {}
 
 }
--- pago-prompt-web/src/app/app-routing.module.ts
+++ pago-prompt-web/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { LoginComponent } from "./login/login.component";
+import { AlarmsComponent } from './alarms/alarms.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: DashboardComponent},
+  { path: 'alarms', component: AlarmsComponent },
+  { path: '', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
