57731e77cf681821551610cdb6402290a10022b0
Fabian Payan <f.payan@uniandes.edu.co>
2024-03-10 07:26:05 -0500

    Se agrega flujo de login
    Co-Authored-By: Esneider Velandia Gonzalez <e.velandia2164@uniandes.edu.co>

--- pago-prompt-web/src/app/app.component.ts
+++ pago-prompt-web/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from "./services/authentication.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pago-prompt-web';
+  imageSrc: string = '../assets/menu/logout.png';
+
+  constructor(public authenticationService: AuthenticationService) { }
+
+  changeImage(image: string) {
+    this.imageSrc = '../assets/menu/' + image;
+  }
+
+  logout() {
+    this.authenticationService.logout();
+  }
 }
