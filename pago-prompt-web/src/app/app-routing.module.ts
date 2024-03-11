a8d026ad4f648251155c1249ce6fd99a4d1637cb
Fabian Payan <f.payan@uniandes.edu.co>
2024-03-11 15:38:53 -0500

    Update app-routing.module.ts
    Se actualiza ruteo del flujo principal
    
    Co-Authored-By: Esneider Velandia Gonzalez <e.velandia2164@uniandes.edu.co>

--- pago-prompt-web/src/app/app-routing.module.ts
+++ pago-prompt-web/src/app/app-routing.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { LoginComponent } from "./login/login.component";
+import { AuthGuard } from "./guards/auth.guard";
 import { AlarmsComponent } from './alarms/alarms.component';
+import { PublicServicesListComponent } from './public-services/public-services-list/public-services-list.component';
+import { PublicServicesCreateComponent } from './public-services/public-services-create/public-services-create.component';
 
 const routes: Routes = [
-  { path: 'home', component: DashboardComponent},
-  { path: 'alarms', component: AlarmsComponent },
+  { path: 'home', component: DashboardComponent, canActivate: [AuthGuard]  },
+  { path: 'alarms', component: AlarmsComponent  },
+  { path: 'public-services/list', component: PublicServicesListComponent  },
+  { path: 'public-services/create', component: PublicServicesCreateComponent  },
   { path: '', component: LoginComponent },
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
