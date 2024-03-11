b0b162c0c54042f0ef7fe5f58451f91049fb6cba
Esneider Velandia Gonzalez <e.velandia2164@uniandes.edu.co>
2024-03-11 15:25:37 -0500

    Update model

---
+++ pago-prompt-web/src/app/shared/public-service/public-service.ts
@@ -0,0 +1,15 @@
+export class PublicService {
+  id: number;
+  name: string;
+  type: string;
+
+  constructor(
+    id: number,
+    name: string,
+    type: string
+  ) {
+    this.id = id;
+    this.name = name;
+    this.type = type;
+  }
+}
